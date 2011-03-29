4fdc6ff6ba635e94fa2eda820f635cbeac81e3df
Steve Yen <steve.yen@gmail.com>
2011-03-28 18:46:51 -0700

    first commit from rails gen

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Instantlove::Application.config.secret_token = '182c963aa17c83fed4e90ed2016181066629af98848ac14ee93b35661e84a8e5f125e267d3ae8f67a34847ca2075311671e5a42e4dc8992dd16bea5499cd866d'
